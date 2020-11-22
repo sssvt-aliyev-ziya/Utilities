1b11f46a679ac3aa2b17990754597a875f516e76
Ziya Aliyev <alieyevziya@sssvt.cz>
2020-11-22 14:17:05 +0100

    TECH Add a new file to the project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyNamespace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
