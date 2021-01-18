6c9d9ecc563c4294573b51c0e050fd9452e93797
Aliyev Ziya <alieyevziya@sssvt.cz>
2021-01-18 23:31:38 +0100

    Added House project created Widow and Door

---
+++ utilities-master/UtilitiesSol/UtilitiesDraw/CanvasForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using UtilitiesDraw.BusinessObjects.HouseBuilding;
+using UtilitiesDraw.PaintersObjects;
+
+
+
+namespace UtilitiesDraw
+{
+
+
+
+    public partial class CanvasForm : Form
+    {
+
+
+
+        private bool isDrawingVisible = false;
+
+
+
+        public CanvasForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //private void CanvasForm_Paint(object sender, PaintEventArgs e)
+        //{
+        //    Graphics g = e.Graphics;
+        //    //Pen pen = new Pen(Color.Black, 1.0f);
+        //    Pen pen = new Pen(Color.Black, 2.0f);
+        //    g.DrawLine(pen, 0, 0, 100, 100);
+        //}
+
+
+
+        private void panelCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (isDrawingVisible)
+            {
+                //Graphics g = e.Graphics;
+                ////Pen pen = new Pen(Color.Black, 1.0f);
+                //Pen pen = new Pen(Color.Black, 2.0f);
+                //g.DrawLine(pen, 0, 0, 100, 100);
+                //pen.Dispose();
+                DrawAll(e.Graphics);
+            }
+        }
+
+
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            this.isDrawingVisible = true;
+            this.panelCanvas.Refresh();
+        }
+
+
+
+        private void buttonHide_Click(object sender, EventArgs e)
+        {
+            this.isDrawingVisible = false;
+            this.panelCanvas.Refresh();
+        }
+
+
+
+        private void CanvasForm_Resize(object sender, EventArgs e)
+        {
+            if (this.isDrawingVisible)
+            {
+                this.panelCanvas.Refresh();
+            }
+        }
+
+
+        private void DrawAll(Graphics g)
+        {
+            BlockOfFlats block = BlockOfFlats.GetKLadviBlockOfFlats();
+            CanvasContext context = new CanvasContext(0, 0, panelCanvas.Width, panelCanvas.Height);
+            block.DrawSelf(g, context);
+            DrawEngine drawEngine = new DrawEngine(block, g, context);
+            drawEngine.DrawAllElements();
+
+            Window window = Window.GetStandardWindow();           
+            window.DrawSelf(g, context);
+            Door door = Door.GetSingleDoor();
+            door.DrawSelf(g, context);
+        }
+
+        private void CanvasForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
